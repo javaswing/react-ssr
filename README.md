c4a70974e12a99c36ae82d1212b6a04cf0643792
javaSwing <zxdlovejava@gmail.com>
2023-11-02 00:32:17 +0800

    feat: first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# React SSR
+
+React SSR 学习项目
+
+## 参考
+
+1. https://nils-mehlhorn.de/posts/typescript-nodejs-react-ssr/
---
+++ client/client.tsx
@@ -0,0 +1,5 @@
+import ReactDOM from "react-dom";
+import App from "./components/app";
+import React from "react";
+
+ReactDOM.hydrate(<App />, document.getElementById("root"));
