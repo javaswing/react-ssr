2722102f82c0da5dca9ddd2914e863ba1a5aec56
javaSwing <zxdlovejava@gmail.com>
2023-11-09 00:10:40 +0800

    fix: fixed react router server error

--- client/client.tsx
+++ client/client.tsx
@@ -1,7 +1,13 @@
 import ReactDOM from "react-dom/client";
 import App from "./app";
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
 
-// @ts-ignore
 // in server render mode, you must be use hydrateRoot method
 // ReactDOM.createRoot(document.getElementById("root"), <App />);
-ReactDOM.hydrateRoot(document.getElementById("root"), <App />);
+ReactDOM.hydrateRoot(
+  document.getElementById("root") as HTMLElement,
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
