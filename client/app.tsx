b2a449f0f58ffc5cd9c730c3ee3854134d491c76
javaswing <zxdlovejava@gmail.com>
2023-11-10 19:40:59 +0800

    feat: add zustand pkg

--- client/app.tsx
+++ client/app.tsx
@@ -2,17 +2,13 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "./components/home";
 import List from "./components/list";
-import { Provider } from "react-redux";
-import store from "./store";
 
 function App() {
   return (
-    <Provider store={store}>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/list" element={<List />} />
-      </Routes>
-    </Provider>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/list" element={<List />} />
+    </Routes>
   );
 }
 
--- client/store.ts
+++ client/store.ts
@@ -1,13 +1,13 @@
-import { configureStore } from "@reduxjs/toolkit";
-import rootReducer from "./redux";
-import logger from "redux-logger";
+import { create } from "zustand";
+import { getPlayListById } from "./service";
 
-const store = configureStore({
-  reducer: rootReducer,
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
-  devTools: process.env.NODE_ENV !== "production",
-});
 
-export type AppDispatch = typeof store.dispatch;
-export type RootState = ReturnType<typeof store.getState>;
-export default store;
+const useListStore = create((set) => ({
+  list: {},
+  fetch: async () => {
+    const response = await getPlayListById();
+    set({ list: response });
+  },
+}));
+
+export { useListStore };
